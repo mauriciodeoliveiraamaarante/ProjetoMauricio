19d323e910eaa7e4db66d2a202de19e890e25f6f
Mauricio Aarante <Mauricio.amarante3@hotmail.com>
2018-08-06 21:59:28 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+//Definindo a variavel nome
+//var nome = "Mauricio de Oliveira Amarante";
+//var x = prompt("Digite um número");
+
+document.write("<img src='http://wwww.google.com.br/google.jpg'>");
